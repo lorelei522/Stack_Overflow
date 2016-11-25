dc503dee214f397b8f975f3b7e2fae0f21c9dc5a
Lorelei Lopez <email": null>
2016-11-25 11:03:13 -0500

    fix question forms and post questions route

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -1,8 +1,13 @@
-def logged_in?
-  !!current_user
-end 
+helpers do
+  def logged_in?
+    !!current_user
+  end
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-def current_user
-  @current_user ||= User.find_by(id: session[:user_id])
-end 
+  def require_login
+    redirect '/sessions/new' unless logged_in?
+  end
+end
