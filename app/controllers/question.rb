f73be9d93a7a270d86331dac8e31f4ed092e5a99
Evan Renaud <email": null>
2016-11-25 10:53:00 -0500

    users are able to answer questions when logged in

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,17 +1,34 @@
 get '/questions' do
   @questions = Question.all
   erb :'/questions/index'
-end 
+end
 
 get '/questions/new' do
   erb:'/questions/new'
 end
 
 
-get '/questions/:id' do 
+get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   @answers = Answer.where(question_id: params[:id])
   erb :'/questions/show'
-end 
+end
 
 
+get '/questions/:id/answers/new' do
+  @question = Question.find_by(id: params[:id])
+  erb :'/answers/new'
+end
+
+post '/questions/:id/answers' do
+  question = Question.find_by(id: params[:id])
+  answer = Answer.new(text: params[:text],
+             question_id: question.id,
+             user_id: current_user.id)
+
+  if answer.save
+    redirect "/questions/#{question.id}"
+  else
+    erb :'/answers/new'
+  end
+end
